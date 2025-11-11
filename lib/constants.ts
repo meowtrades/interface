ab57a8285b5f1426f88cd4cf30611898165665c2
Cygnus <cygnusdev04@gmail.com>
2025-11-11 21:46:47 +0530

    fixed staging branch

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,11 @@
 /** @format */
 
-export const USDT_DENOM = "peggy0x87aB3B4C8661e07D6372361211B96ed4Dc36B1B5";
+export const USDT_DENOM_TESTNET =
+  "peggy0x87aB3B4C8661e07D6372361211B96ed4Dc36B1B5";
+
+// Mainnet USDT (MTS - MultiVM Token Standard)
+export const USDT_DENOM_MAINNET =
+  "peggy0xdAC17F958D2ee523a2206206994597C13D831ec7";
+
+// Legacy export for backward compatibility (defaults to testnet)
+export const USDT_DENOM = USDT_DENOM_TESTNET;
